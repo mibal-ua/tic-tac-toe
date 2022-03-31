ca921c17b3d52a66c5e09867c7195e56936ddf78
mibal_ua <9mohapx9@gmail.com>
2022-03-31 12:36:54 +0300

    Add model classes

--- src/ua/mibal/tictactoe/model/Cell.java
+++ src/ua/mibal/tictactoe/model/Cell.java
@@ -20,23 +20,22 @@ package ua.mibal.tictactoe.model;
  * @author Michael Balakhon
  * @link http://t.me/mibal_ua
  */
-public class GameTable {
+public class Cell {
 
-    private final char[][] table = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-    };
+    private final int row;
 
-    public boolean isEmpty(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()] == ' ';
+    private final int col;
+
+    public Cell(final int row, final int col) {
+        this.row = row;
+        this.col = col;
     }
 
-    public char getSign(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()];
+    public int getRow() {
+        return row;
     }
 
-    public void setSign(final Cell cell, final char sign) {
-        table[cell.getRow()][cell.getCol()] = sign;
+    public int getCol() {
+        return col;
     }
 }
