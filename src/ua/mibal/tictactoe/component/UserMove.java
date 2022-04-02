5352e11a7881b1d6ebd754de08a9cbdcc7e4a267
mibal_ua <9mohapx9@gmail.com>
2022-04-02 15:50:27 +0300

    Implement UserMove component

--- src/ua/mibal/tictactoe/component/UserMove.java
+++ src/ua/mibal/tictactoe/component/UserMove.java
@@ -16,14 +16,56 @@
 
 package ua.mibal.tictactoe.component;
 
+import ua.mibal.tictactoe.model.Cell;
 import ua.mibal.tictactoe.model.GameTable;
+
+import java.util.Scanner;
 
 /**
  * @author Michael Balakhon
  * @link http://t.me/mibal_ua
  */
 public class UserMove {
- public void make(final GameTable gameTable) {
+    public void make(final GameTable gameTable) {
+        while (true) {
+            System.out.println("Please type number between 1 and 9:");
+            int user = new Scanner(System.in).nextInt();
+            if (requestIsCorrect(user)) {
+                Cell cell = createCellByNumber(user);
+                if (gameTable.isEmpty(cell)) {
+                    gameTable.setSign(cell, 'X');
+                    return;
+                }
+                System.out.print(user + " cell is not empty. ");
+                continue;
+            }
+            System.out.print(user + " is not correct number. ️");
+        }
+        /*
+        7 - 0 0
+        8 - 0 1
+        9 - 0 2
 
- }
+        int row = 2 - (user - 1)/3;
+        int col = (user - 1)%3;
+
+        4 - 1 0
+        5 - 1 1
+        6 - 1 2
+
+        1 - 2 0
+        2 - 2 1
+        3 - 2 2
+         */
+    }
+
+    private Cell createCellByNumber(final int user) {
+        int row = 2 - (user - 1) / 3;
+        int col = (user - 1) % 3;
+        return new Cell(row, col);
+    }
+
+    private boolean requestIsCorrect(final int user) {
+        return 1 <= user && user <= 9;
+    }
 }
