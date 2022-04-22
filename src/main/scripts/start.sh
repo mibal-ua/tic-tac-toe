655a48c60027c17d21495c242ba5134e248b752d
mibal_ua <9mohapx9@gmail.com>
2022-04-22 12:35:35 +0200

    Fix start.sh script

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+cd "$(dirname "$0")" || exit
 
 java -jar ${project.build.finalName}.jar
 echo "Press enter to continue..."
