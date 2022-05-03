007f7e4354da1a6758279448a04e9c40badce64b
mibal_ua <9mohapx9@gmail.com>
2022-05-03 18:00:28 +0200

    Add CommandLineArgumentParser component

--- src/main/java/ua/mibal/tictactoe/component/GameFactory.java
+++ src/main/java/ua/mibal/tictactoe/component/GameFactory.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2022. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package ua.mibal.tictactoe.component;
 
 import ua.mibal.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 import ua.mibal.tictactoe.model.Player;
 import ua.mibal.tictactoe.model.PlayerType;
 
-import static ua.mibal.tictactoe.model.PlayerType.COMPUTER;
 import static ua.mibal.tictactoe.model.PlayerType.USER;
 import static ua.mibal.tictactoe.model.Sign.O;
 import static ua.mibal.tictactoe.model.Sign.X;
@@ -20,33 +36,10 @@ public class GameFactory {
     private final PlayerType player2Type;
 
     public GameFactory(final String[] args) {
-        PlayerType player1Type = null;
-        PlayerType player2Type = null;
-
-        for (final String arg : args) {
-            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
-                if (player1Type == null) {
-                    player1Type = PlayerType.valueOf(arg.toUpperCase());
-                } else if (player2Type == null) {
-                    player2Type = PlayerType.valueOf(arg.toUpperCase());
-                } else {
-                    System.err.println("Unsupported command line argument: '" + arg + "'");
-                }
-            } else {
-                System.err.println("Unsupported command line argument: '" + arg + "'");
-            }
-        }
-
-        if (player1Type == null) {
-            this.player1Type = USER;
-            this.player2Type = COMPUTER;
-        } else if (player2Type == null) {
-            this.player1Type = USER;
-            this.player2Type = player1Type;
-        } else {
-            this.player1Type = player1Type;
-            this.player2Type = player2Type;
-        }
+        final CommandLineArgumentParser.PlayerTypes playerTypes =
+                new CommandLineArgumentParser(args).parse();
+        this.player1Type = playerTypes.getPlayer1Type();
+        this.player2Type = playerTypes.getPlayer2Type();
     }
 
     public Game create() {
